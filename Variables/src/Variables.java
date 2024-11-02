d8a3418a29db9e49771d0d7d0f84e465471fac1a
JmcaDev <jmcavzla@gmail.com>
2024-11-02 13:00:35 -0400

    Variables

---
+++ Variables/src/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Variables
+        int edad = 24;
+        double precio = 300.5;
+        boolean disponible = true; // true-false
+        char genero;
+        genero = 'M'; // M-Masculino / F-Femenino
+
+        //Acceder a las variables
+        System.out.println(edad);
+
+        //Modificar el valor
+        edad = 25;
+        System.out.println(edad);
+
+        //Imprimir resto de variables
+        System.out.println(precio);
+        System.out.println(disponible);
+        System.out.println(genero);
+    }
+}
